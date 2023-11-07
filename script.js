8c2ef4bffb326713d9a3c0946522d304c2feedcb
eefuwape <eefuwape@gmail.com>
2023-11-07 11:20:51 -0500

    more additions

---
+++ script.js
@@ -0,0 +1,35 @@
+// Data
+
+class Player {
+    constructor() {
+        this.name
+        this.career
+        this.salary
+        this.bankAccount
+        this.loan
+        this.lifeTiles
+        this.car
+        this.children
+    }
+}
+
+class Cards {
+    constructor () {
+        this.career
+        this.salary
+        this.stock
+        this.insurance
+    }
+}
+
+class spaces {
+    constructor() {
+        this.red
+        this.orange
+        this.green
+        this.retire
+        this.rich
+        this.poor
+        this.start
+    }
+}
